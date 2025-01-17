18ec3e7876e63852ecaaf8033e0e17c05beb15ce
RoiFa <131765910+RoiFa@users.noreply.github.com>
2025-01-17 22:06:49 +0100

    Add files via upload

---
+++ multimedia/src/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("trabajo.fxml"));
+        Parent root = (Parent) loader.load();
+        Screen controller = loader.getController();
+        controller.setMainWindow(primaryStage);
+        primaryStage.setTitle("mis bolas");
+        primaryStage.setScene(new Scene(root,800,800));
+        primaryStage.show();
+    }
+}
